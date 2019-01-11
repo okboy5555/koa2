dd15dcf98835c421e2df25c7bf69293d97a76914
miaojiasheng <miaojiasheng@58ganji.com>
2019-01-11 15:46:18 +0800

    init node

---
+++ app.js
@@ -0,0 +1,47 @@
+// app.js
+
+const path = require('path')
+const Koa = require('koa2');
+const router = require('koa-router');
+var cors = require('koa-cors');
+const bodyParser = require('koa-bodyparser');
+const config = require('./config.js');
+const server = require('koa-static');
+const jwt = require('jsonwebtoken')
+
+require('./mysql.js');
+
+const app = new Koa();
+
+app.use(server(
+  path.join(__dirname , './public')
+)) // 设置静态文件
+
+app.use(cors({
+      origin: 'http://localhost:8080',  // 允许 loclhost:8080 访问
+      exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+      maxAge: 5,
+      credentials: true,
+      allowMethods: ['GET', 'POST'],
+      allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  })
+); // 设置跨域
+
+app.use(bodyParser({
+  formLimit: '1mb'
+})) // ctx body 中间件
+
+// app.use(require('./routers/signUp.js').routes()) // 注册
+// app.use(require('./routers/signIn.js').routes()) // 登录
+
+// app.use(require('./routers/createPosts.js').routes()) // 新建文章
+// app.use(require('./routers/postsList.js').routes()) // 搜索文章
+// app.use(require('./routers/postDetail.js').routes()) // 文章detail
+// app.use(require('./routers/updatePosts.js').routes()) // 修改文章
+
+// app.use(require('./routers/createComment.js').routes()) // 添加留言
+// app.use(require('./routers/commentList.js').routes()) // 获取留言
+
+app.listen(config.port) // 监听端口
+
+console.log('listen in localhost:' + config.port)
---
+++ config.js
@@ -0,0 +1,10 @@
+// 数据库配置
+module.exports = {
+    mysql: {
+        host: 'localhost', // 地址
+        user: 'root', // 用户账号
+        password: '123456', // 用户密码
+        database: 'miaotuan' // 库
+    },
+    port: 3001 // 监听端口
+}
